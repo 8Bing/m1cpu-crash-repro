3463374ad4381cb1ff1065ca5cbf1aff37e8fde7
Bing Lo <binglo@Bings-MacBook-Pro.local>
2026-03-16 14:08:07 +1100

    Minimal repro for go-m1cpu crash on Apple M5 Pro

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/8Bing/m1cpu-crash-repro
+
+go 1.26.1
+
+require github.com/shoenig/go-m1cpu v0.1.6 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+// main.go
+package main
+
+import (
+    "fmt"
+
+    _ "github.com/shoenig/go-m1cpu"
+)
+
+func main() {
+    fmt.Println("Program started, go-m1cpu imported.")
+}
+
